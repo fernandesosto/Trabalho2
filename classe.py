1a3b7a3418c2765c2309b2eb6e7ec5b5e2e4b851
fernandesosto <vanesaosto0924@gmail.com>
2022-12-15 21:08:20 -0300

    Criando os metodos criar e editar

--- classe.py
+++ classe.py
@@ -1,3 +1,22 @@
-class Produto:
-    print ("nova classe")
-    print ("teste")
+from interacaojason import lerJson, gravarJson
+
+class Produto :
+    sku : int
+    nome : str
+    preco : float
+    categoria : str
+    condicao : str
+
+def criar(dado: dict) -> dict:
+    dados = lerJson()
+    dado['sku'] 
+    dados.append(dado)
+    gravarJson(dados)
+
+
+def editar(dado: dict) -> None:
+    dados = lerJson()
+    for i, data in enumerate(dados):
+        if data['sku'] == dado['sku']:
+            dados[i] = dado
+    gravarJson(dados)
